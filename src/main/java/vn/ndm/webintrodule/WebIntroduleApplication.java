cc28da192a0e73405841f2adfff0244ba1a5397d
manhnd <manhnd@neo.vn>
2023-02-16 11:07:22 +0700

    new commit

---
+++ src/main/java/vn/ndm/webintrodule/WebIntroduleApplication.java
@@ -0,0 +1,13 @@
+package vn.ndm.webintrodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebIntroduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebIntroduleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/ndm/webintrodule/WebIntroduleApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.ndm.webintrodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebIntroduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
