6dfb43cedd3bd190611766f66d44b6f3a5817827
manhnd <manhnd@neo.vn>
2023-02-20 10:30:44 +0700

    clean code

--- src/main/java/vn/ndm/webintrodule/controller/HomeController.java
+++ src/main/java/vn/ndm/webintrodule/controller/HomeController.java
@@ -7,12 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/home")
-    public String home(Model model) {
-        // Thêm các attribute cần thiết cho trang home
-        return "home/home";
-    }
-
     @GetMapping("/about")
     public String about(Model model) {
         // Thêm các attribute cần thiết cho trang home
