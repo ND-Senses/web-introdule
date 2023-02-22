6bb338d6ea959dee027acca12a466a3633077c3f
manhnd <manhnd@neo.vn>
2023-02-22 17:22:07 +0700

    custome login & signup v2

--- src/main/java/vn/ndm/webintrodule/controller/HomeController.java
+++ src/main/java/vn/ndm/webintrodule/controller/HomeController.java
@@ -6,28 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-
-    @GetMapping("/about")
-    public String about(Model model) {
+    @GetMapping("/home")
+    public String knightOne(Model model) {
         // Thêm các attribute cần thiết cho trang home
-        return "home/about";
-    }
-
-    @GetMapping("/contact")
-    public String contact(Model model) {
-        // Thêm các attribute cần thiết cho trang home
-        return "home/contact";
-    }
-
-    @GetMapping("/services")
-    public String services(Model model) {
-        // Thêm các attribute cần thiết cho trang home
-        return "home/services";
-    }
-
-    @GetMapping("/index")
-    public String index(Model model) {
-        // Thêm các attribute cần thiết cho trang home
-        return "home/index";
+        return "knight-one/index";
     }
 }
